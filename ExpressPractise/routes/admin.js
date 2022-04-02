389f233ecfd7995523ded5b9d7fd520f06fc9b75
Mythireyan <mythireyan@Mythireyans-MacBook-Air.local>
2022-04-02 14:42:34 +0530

    pug template engine

--- ExpressPractise/routes/admin.js
+++ ExpressPractise/routes/admin.js
@@ -6,13 +6,16 @@ const rootDir = require("../utils/rootPath");
 
 const router = express.Router();
 
+const productsArr = [];
+
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "addProduct.html"));
+  res.render("addProduct", { pageTitle: "Add Product", path: "addProduct" });
 });
 
 router.post("/add-product", (req, res, next) => {
+  productsArr?.push({ title: req?.body?.title });
   res.redirect("/");
-  console.log(req.body);
 });
 
-module.exports = router;
+exports.router = router;
+exports.productsArr = productsArr;
--- ExpressPractise/routes/shop.js
+++ ExpressPractise/routes/shop.js
@@ -4,10 +4,13 @@ const express = require("express");
 
 const rootDir = require("../utils/rootPath");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData?.productsArr;
+  res.render("shop", { prods: products, docTitle: "Shop", path: "shop" });
 });
 
 module.exports = router;
